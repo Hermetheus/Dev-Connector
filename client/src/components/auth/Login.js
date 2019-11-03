0238b80d6e3a008598c4e2ab0faa7d1d49fa1b37
Hermetheus <allanmolson@gmail.com>
2019-11-03 12:17:13 -0600

    Login Reducer/Auth & Proxy Fix

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
 	const [formData, setFormData] = useState({
 		email: '',
 		password: ''
@@ -14,7 +17,7 @@ const Login = () => {
 
 	const onSubmit = async e => {
 		e.preventDefault();
-		console.log('SUCCESS');
+		login(email, password);
 	};
 
 	//  Changing this to redux, example of a request to MONGODB
@@ -56,6 +59,11 @@ const Login = () => {
 
 	// this.setState
 
+	//Redirect if Logged in
+	if (isAuthenticated) {
+		return <Redirect to='/dashboard' />;
+	}
+
 	return (
 		<Fragment>
 			<h1 className='large text-primary'>Sign In</h1>
@@ -92,4 +100,17 @@ const Login = () => {
 	);
 };
 
-export default Login;
+Login.propTypes = {
+	login: PropTypes.func.isRequired,
+	isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+	isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+	mapStateToProps,
+
+	{ login, mapStateToProps }
+)(Login);
