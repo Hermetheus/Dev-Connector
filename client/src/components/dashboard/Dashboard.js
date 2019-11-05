4871040721e304c123b56a8e9d968e09525ecdf3
Hermetheus <allanmolson@gmail.com>
2019-11-04 22:26:40 -0600

    Add Education, Add Experience, Spinner & More

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { getCurrentProfile } from '../../actions/profile';
+import DashboardActions from './DashboardActions';
+import Spinner from '../layout/Spinner';
 
-const Dashboard = () => {
-	return <div>Dashboard</div>;
+const Dashboard = ({
+	getCurrentProfile,
+	auth: { user },
+	profile: { profile, loading }
+}) => {
+	useEffect(() => {
+		getCurrentProfile();
+	}, []);
+	//		if profile is null && still loading then show spinner
+	return loading && profile === null ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<h1 className='large text-primary'>Dashboard</h1>
+			<p className='lead'>
+				<i className='fas fa-user'></i> Welcome {user && user.name}
+			</p>
+			{profile !== null ? (
+				<Fragment>
+					<DashboardActions />
+				</Fragment>
+			) : (
+				<Fragment>
+					<p>You have not yet setup a profile, please add some info</p>
+					<Link to='/create-profile' className='btn btn-primary my-1'>
+						Create Profile
+					</Link>{' '}
+				</Fragment>
+			)}
+		</Fragment>
+	);
 };
 
-export default Dashboard;
+Dashboard.propTypes = {
+	getCurrentProfile: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth,
+	profile: state.profile
+});
+
+export default connect(
+	mapStateToProps,
+	{ getCurrentProfile }
+)(Dashboard);
